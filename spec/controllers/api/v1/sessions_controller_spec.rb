30ad1d232399a5ac39177f5d6b626e7349ab88ad
Shaiju Edakulangara <shaiju.edakulangara@rubyians.com>
2019-02-24 00:10:03 +0530

    Added test cases for api validate_token

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -37,4 +37,37 @@ describe Api::V1::SessionsController do
       it { should respond_with 401 }
     end
   end
+
+
+  describe 'POST #validate_token' do
+    before(:each) do
+      @user = FactoryBot.create(:user, password: '12345678', password_confirmation: '12345678')
+    end
+
+    context 'when the credentials are correct' do
+      before(:each) do
+        api_authorization_header JsonWebToken.encode(user_id: @user.id)
+        post :validate_token
+      end
+
+      it 'returns the user record corresponding to the given auth token' do
+        expect(json_response[:user][:email]).to eql @user.email
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context 'when the credentials are incorrect' do
+      before(:each) do
+        api_authorization_header "invalid token"
+        post :validate_token
+      end
+
+      it 'returns a json with an error' do
+        expect(json_response[:error]).to eql 'Token invalid'
+      end
+
+      it { should respond_with 401 }
+    end
+  end
 end
