413438f4ca21e14831ce5ff79fdf8ac1ead0765f
Shaiju Edakulangara <shaiju.edakulangara@rubyians.com>
2019-03-14 01:20:31 +0530

    Added dot env gem for envirnment variable settings

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -5,7 +5,7 @@ Swagger::Docs::Config.register_apis(
   '1.0' => {
     api_file_path: 'public/',
     # do not forget to change this when not running in development mode
-    base_path: 'http://localhost:3000',
+    base_path: ENV['HOST_URL'],
     clean_directory: true,
     base_api_controller: ActionController::Base,
     attributes: {
